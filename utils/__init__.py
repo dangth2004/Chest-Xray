677fd5767261a745b54306ae6a08511cb68988a4
dangth2004 <trinhhaidang2004@gmail.com>
2025-07-24 16:37:53 +0700

    Update utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .preprocessing_data import read_dicom_image, resize_image_with_padding, draw_bounding_boxes, print_dicom_data
+from .preprocessing_data import read_dicom_image, draw_bounding_boxes, print_dicom_data
 from .read_and_split_data import split_data
