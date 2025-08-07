e77a075abbab0048499166d729339954348245bc
dangth2004 <trinhhaidang2004@gmail.com>
2025-08-07 23:03:53 +0700

    Update models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,2 @@
-from .train_and_evaluate_model import train_model, evaluate_model
 from .my_faster_rcnn import model_faster_rcnn
-from .prepare_data import load_data, data_transform
+from .my_resnet import MyResNet
