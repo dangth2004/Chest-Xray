cf7546e3fcc75de87101e563dcc2ad1eece51be7
dangth2004 <trinhhaidang2004@gmail.com>
2025-08-11 11:20:30 +0700

    Update my_faster_rcnn.py

--- models/my_faster_rcnn.py
+++ models/my_faster_rcnn.py
@@ -1,22 +1,38 @@
 import torch
+from torch import nn
 from torchvision.models.detection import FasterRCNN
 from torchvision.models import resnet50, ResNet50_Weights, resnet101, ResNet101_Weights
 from torchvision.models.detection.backbone_utils import _resnet_fpn_extractor, _validate_trainable_layers
 
 
-def get_backbone(backbone='resnet50'):
-    if backbone == 'resnet50':
+class MyFasterRCNN(nn.Module):
+    def __init__(self, backbone_name='resnet50', num_classes=15):
+        super(MyFasterRCNN, self).__init__()
+        self.backbone_name = backbone_name
+        self.num_classes = num_classes
+        self.model = self._build_model()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def _build_model(self):
+        return FasterRCNN(backbone=self._get_backbone(), num_classes=self.num_classes,
+                          image_mean=[0.456], image_std=[0.224])
+
+    @staticmethod
+    def _get_backbone():
         # Using ResNet-50 as backbone
-        backbone = resnet50(weights=None)
+        backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+        resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+
         # It's grayscale image so the first convolutional layer take in_channels=1
         backbone.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7, 7),
                                          stride=(2, 2), padding=(3, 3), bias=False)
 
-        resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
-        state_dict = resnet.state_dict()
-        del state_dict['conv1.weight']
-
-        backbone.load_state_dict(state_dict, strict=False)
+        with torch.no_grad():
+            pretrained_weights = resnet.conv1.weight
+            mean_pretrained_weights = pretrained_weights.mean(dim=1, keepdim=True)
+            backbone.conv1.weight.copy_(mean_pretrained_weights)
 
         # Add FPN model for ResNet-50
         trainable_backbone_layers = _validate_trainable_layers(is_trained=True,
@@ -25,33 +41,3 @@ def get_backbone(backbone='resnet50'):
         backbone = _resnet_fpn_extractor(backbone, trainable_layers=trainable_backbone_layers)
 
         return backbone
-
-    elif backbone == 'resnet101':
-        # Using ResNet-101 as backbone
-        backbone = resnet101(weights=None)
-        # It's grayscale image so the first convolutional layer take in_channels=1
-        backbone.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7, 7),
-                                         stride=(2, 2), padding=(3, 3), bias=False)
-
-        resnet = resnet101(weights=ResNet101_Weights.DEFAULT)
-        state_dict = resnet.state_dict()
-        del state_dict['conv1.weight']
-
-        backbone.load_state_dict(state_dict, strict=False)
-
-        # Add FPN model for ResNet-101
-        trainable_backbone_layers = _validate_trainable_layers(is_trained=True,
-                                                               trainable_backbone_layers=5,
-                                                               max_value=5, default_value=3)
-        backbone = _resnet_fpn_extractor(backbone, trainable_layers=trainable_backbone_layers)
-
-        return backbone
-    else:
-        raise NotImplementedError
-
-
-def model_faster_rcnn(device, backbone='resnet50', num_classes=15):
-    backbone = get_backbone(backbone)
-    model = FasterRCNN(backbone=backbone, num_classes=num_classes,
-                       image_mean=[0.456], image_std=[0.224]).to(device)
-    return model
